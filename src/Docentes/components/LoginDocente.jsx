bdf6a1150efa714055275d863380348a76dbd5d3
Fernando Garcia <gfernandoentique37@gmail.com>
2023-06-21 01:06:22 -0600

    Primera aplicación de Estilos

--- src/Docentes/components/LoginDocente.jsx
+++ src/Docentes/components/LoginDocente.jsx
@@ -1,71 +1,88 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 // import 'bootstrap/dist/css/bootstrap.css'
 // import axios from 'axios'
 // import { postData } from './helpers/fetchApi'
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 // import { validation } from './LoginValidation'
-
-
-
+import "../../Assets/styles/styles-forms/Forms-styles.css";
+import "../../Assets/styles/styles-landing/Footer-styles.css";
 
 export const LoginDocente = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
 
-    const [values, setValues] = useState({
-        email: '',
-        password: '',
-    });
+  const [error, setError] = useState({});
 
-    const [error, setError] = useState({})
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setError(validation(values));
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        setError(validation(values))
+    // axios.post('http://localhost:8081/login', { email, password })
+    //     .then( navgiate('/home'))
+    //     .catch(err => console.log(err));
+    // postData(email, password);
+  };
 
-        // axios.post('http://localhost:8081/login', { email, password })
-        //     .then( navgiate('/home'))
-        //     .catch(err => console.log(err));
-        // postData(email, password);
-    }
+  const handleInput = (event) => {
+    setValues((prev) => ({ ...prev, [event.target.name]: event.target.value }));
+  };
 
-    const handleInput = (event) => {
-        setValues(prev => ({ ...prev, [event.target.name]: event.target.value }))
-    }
+  return (
+    <div className="d-flex vh-100 justify-content-center align-items-center bg-primary ">
+      <div className="form">
+        <br />
+        <h2>Inicio Sesión del Docente</h2>
+        <br />
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Correo Electrónico</strong>
+            </label>
+            <input
+              type="email"
+              placeholder="ejemplo@gmail.com"
+              className="rounded-0 form-control"
+              name="email"
+              onChange={handleInput}
+            />
+            {error.email && <span className="text-danger">{error.email}</span>}
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password">
+              <strong>Contraseña</strong>
+            </label>
+            <input
+              type="password"
+              placeholder="Ingrese su contraseña"
+              className="rounded-0 form-control"
+              name="password"
+              onChange={handleInput}
+            />
+            {error.password && (
+              <span className="text-danger">{error.password}</span>
+            )}
+          </div>
+          <p>Recuerda que si eres docente o administrador <br /> tu correo termina en @unah.edu.hn</p>
+          <button type="submit" className="btn btn-success form-control">
+            Iniciar Sesión
+          </button>
+          <br />
+          <br />
+          <p>Al hacer click estas aceptando nuestros términos y condiciones</p>
+          <Link
+            to="/signup"
+            className="btn btn-default border w-100 bg-light text-decoration-none"
+          >
+            Regístrate
+          </Link>
+        </form>
+      </div>
 
-    return (
-        <div className='d-flex vh-100 justify-content-center align-items-center bg-primary '>
-            <div className='p-3 bg-white w-25 '>
-                <h2>Inicio sesión de docente</h2>
-                <form onSubmit={handleSubmit}>
-                    <div className='mb-3'>
-                        <label htmlFor="email"><strong>Email</strong></label>
-                        <input type="email"
-                            placeholder='ejemplo@gmail.com'
-                            className='rounded-0 form-control'
-                            name='email'
-                            onChange={handleInput} />
-                        {error.email && <span className='text-danger'>{error.email}</span>}
-                    </div>
-                    <div className='mb-3'>
-                        <label htmlFor="password"><strong>Password</strong></label>
-                        <input type="password"
-                            placeholder='Ingrese su contraseña'
-                            className='rounded-0 form-control'
-                            name='password'
-                            onChange={handleInput} />
-                        {error.password && <span className='text-danger'>{error.password}</span>}
-                    </div>
-                    <button
-                        type='submit'
-                        className='btn btn-success form-control'>
-                        Log In
-                    </button>
-                    <p>You are agree to aour terms and polices</p>
-                    <Link to="/signup"
-                        className='btn btn-default border w-100 bg-light text-decoration-none'>
-                        Create acount
-                    </Link>
-                </form>
-            </div>
-        </div>
-    )
-}
+      <div className="footer z-n1 position-absolute bottom-0 start-50 translate-middle-x">
+        <img src="../src/Assets/img/footer-bg.png" alt="" />
+      </div>
+    </div>
+  );
+};
--- src/IU/pages/LandingPage.jsx
+++ src/IU/pages/LandingPage.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
-
+import "../../Assets/styles/styles-landing/Landin-styles.css";
+import "../../Assets/styles/styles-landing/Footer-styles.css";
 export const LandingPage = () => {
   return (
-    <div>LandingPage</div>
-  )
-}
+    <>
+      <ul className="z-0 position-absolute top-50 start-50 translate-middle">
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+
+      <div className="footer z-n1 position-absolute bottom-0 start-50 translate-middle-x">
+        <img src="../src/Assets/img/footer-bg.png" alt="" />
+      </div>
+    </>
+  );
+};
