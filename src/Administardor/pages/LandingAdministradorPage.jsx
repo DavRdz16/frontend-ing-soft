2c22ba61a7e029117c50649a1b05ed69d2ab3956
Fernando Garcia <gfernandoentique37@gmail.com>
2023-06-23 16:33:06 -0600

    primer commit estilos

--- src/Administardor/pages/LandingAdministradorPage.jsx
+++ src/Administardor/pages/LandingAdministradorPage.jsx
@@ -1,37 +1,49 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../../Assets/styles/styles-admin/Admin-home.css";
 
 export const LandingAdministradorPage = () => {
-
-    const navigate = useNavigate()
-    const onNavRegistroDocente = () => {
-        navigate('/administrador/RegistroDocente')
-    }
-    const onNavRegistroEstudiante = () => {
-        navigate('/administrador/RegistroEstudiante')
-    }
-    return (
-
-        <>
-        <h1>Necesito que bajes la informacion porque se esconde en el navbar</h1>
-        <h1>Bienvenido al modulo de administrador</h1>
-        
-            <div>
-                <button onClick={onNavRegistroDocente}>Registrar docentes</button>
-                <hr />
-            </div>
-            <div>
-                <button onClick={onNavRegistroEstudiante}>Registrar Estudiantes</button>
-                <hr />
-            </div>
-            <div>
-                <button> Seleccionar jefe de departamento</button>
-                <hr />
-            </div>
-            <div>
-                <button> Seleccionar coordinador de carrera</button>
-                <hr />
-            </div>
-        </>
-    )
-}
+  const navigate = useNavigate();
+  const onNavRegistroDocente = () => {
+    navigate("/administrador/RegistroDocente");
+  };
+  const onNavRegistroEstudiante = () => {
+    navigate("/administrador/RegistroEstudiante");
+  };
+  return (
+    <>
+      <div className="mt-6 d-flex flex-column align-items-center justify-content-center">
+        <div className="btn'group">
+          <div>
+            <button
+              className="btn btn-primary m-1"
+              onClick={onNavRegistroDocente}
+            >
+              Registrar docentes
+            </button>
+          </div>
+          <div>
+            <button
+              className="btn btn-primary m-1"
+              onClick={onNavRegistroEstudiante}
+            >
+              Registrar Estudiantes
+            </button>
+          </div>
+          <div>
+            <button className="btn btn-primary m-1">
+              {" "}
+              Seleccionar jefe de departamento
+            </button>
+          </div>
+          <div>
+            <button className="btn btn-primary m-1">
+              {" "}
+              Seleccionar coordinador de carrera
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/Docentes/components/LoginDocente.jsx
+++ src/Docentes/components/LoginDocente.jsx
@@ -1,75 +1,99 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import { Link, useNavigate } from 'react-router-dom'
-import { validacionRegisto } from '../../Administardor/helpers/ValidacionRegistro'
-
+import React, { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { validacionRegisto } from "../../Administardor/helpers/ValidacionRegistro";
+import "../../Assets/styles/styles-forms/Forms-styles.css";
+import "../../Assets/styles/styles-landing/Footer-styles.css";
 
 export const LoginDocente = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    const [values, setValues] = useState({
-        email: '',
-        password: '',
-    });
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
 
-    const [error, setError] = useState({})
+  const [error, setError] = useState({});
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        setError(validacionRegisto(values))
-        const { email, password } = values;
-        axios.post('http://localhost:8081/login/docente', { email, password })
-            .then(res => {
-                const { login, usuario, token } = res.data;
-                if (login) {
-                    localStorage.setItem("token", token)
-                    localStorage.setItem("login", login)
-                    console.log({ login, usuario, token });
-                    alert('Inicio sesion exitoso')
-                    navigate('/docente/home')
-                } else {
-                    console.log({ login, usuario });
-                    alert('El usuario no existe')
-                }
-            })
-            .catch(err => console.log(err));
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setError(validacionRegisto(values));
+    const { email, password } = values;
+    axios
+      .post("http://localhost:8081/login/docente", { email, password })
+      .then((res) => {
+        const { login, usuario, token } = res.data;
+        if (login) {
+          localStorage.setItem("token", token);
+          localStorage.setItem("login", login);
+          console.log({ login, usuario, token });
+          alert("Inicio sesion exitoso");
+          navigate("/docente/home");
+        } else {
+          console.log({ login, usuario });
+          alert("El usuario no existe");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
 
-    const handleInput = (event) => {
-        setValues(prev => ({ ...prev, [event.target.name]: event.target.value }))
-    }
+  const handleInput = (event) => {
+    setValues((prev) => ({ ...prev, [event.target.name]: event.target.value }));
+  };
 
-    return (
-        <div className='d-flex vh-100 justify-content-center align-items-center bg-primary '>
-            <div className='p-3 bg-white w-25 '>
-                <h2>Inicio sesión de docente</h2>
-                <form onSubmit={handleSubmit}>
-                    <div className='mb-3'>
-                        <label htmlFor="email"><strong>Correo Electronico</strong></label>
-                        <input type="email"
-                            placeholder='ejemplo@gmail.com'
-                            className='rounded-0 form-control'
-                            name='email'
-                            onChange={handleInput} />
-                        {error.email && <span className='text-danger'>{error.email}</span>}
-                    </div>
-                    <div className='mb-3'>
-                        <label htmlFor="password"><strong>Contraseña</strong></label>
-                        <input type="password"
-                            placeholder='Ingrese su contraseña'
-                            className='rounded-0 form-control'
-                            name='password'
-                            onChange={handleInput} />
-                        {error.password && <span className='text-danger'>{error.password}</span>}
-                    </div>
-                    <button
-                        type='submit'
-                        className='btn btn-success form-control'>
-                        Iniciar sesión
-                    </button>
-
-                </form>
+  return (
+    <>
+      <div className="d-flex vh-100 justify-content-center align-items-center bg-primary ">
+        <div className="form">
+          <br />
+          <h2>Inicio Sesión del Docente</h2>
+          <br />
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label htmlFor="email">
+                <strong>Correo Electrónico</strong>
+              </label>
+              <input
+                type="email"
+                placeholder="ejemplo@gmail.com"
+                className="rounded-0 form-control"
+                name="email"
+                onChange={handleInput}
+              />
+              {error.email && (
+                <span className="text-danger">{error.email}</span>
+              )}
             </div>
+            <div className="mb-3">
+              <label htmlFor="password">
+                <strong>Contraseña</strong>
+              </label>
+              <input
+                type="password"
+                placeholder="Ingrese su contraseña"
+                className="rounded-3 form-control"
+                name="password"
+                onChange={handleInput}
+              />
+              {error.password && (
+                <span className="text-danger">{error.password}</span>
+              )}
+            </div>
+            <p>
+              Recuerda que si eres docente o administrador <br /> tu correo
+              termina en @unah.edu.hn
+            </p>
+            <button type="submit" className="btn btn-success form-control">
+              Iniciar Sesión
+            </button>
+            <br />
+            <br />
+            <p>
+              Al hacer click estas aceptando nuestros términos y condiciones
+            </p>
+          </form>
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
